efe1077c7e7fdf8fce5dc70e9db1730d29a036c8
Jack Lanchantin <jacklanchantin@gmail.com>
2023-02-21 13:45:11 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,15 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Jack Lanchantin - Meta</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Jack Lanchantin" />
+    
+        Jack Lanchantin is a researcher at Meta NY. He did his bachelors at The State University of New York at Binghamton, and masters and PhD at UVA. 
+
         <p className="description">
         </p>
       </main>
